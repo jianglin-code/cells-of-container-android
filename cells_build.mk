72c2c15c8df00a3730d214083970f39c81cfdbbd
jianglin <61100700+jianglin-code@users.noreply.github.com>
2021-10-31 14:52:14 +0800

    Update cells_build.mk

--- cells_build.mk
+++ cells_build.mk
@@ -1,5 +1,2 @@
-##########################################################
-# Library for ART-specific functions
-##########################################################
 
 PRODUCT_PACKAGES += busybox celld cellc cellsservice switchsystem cellssync cellsupdownvm cswitch
